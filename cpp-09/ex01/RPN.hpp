5121fb652b8d3c5ce1596891776c5b63ee40b57e
marzianegro <ngr.marzia@gmail.com>
2024-02-11 23:48:51 +0100

    added files for ex01

--- cpp-09/ex01/RPN.hpp
+++ cpp-09/ex01/RPN.hpp
@@ -6,10 +6,25 @@
 /*   By: mnegro <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 16:40:38 by mnegro            #+#    #+#             */
-/*   Updated: 2024/02/07 16:40:44 by mnegro           ###   ########.fr       */
+/*   Updated: 2024/02/09 11:10:41 by mnegro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
+#include <stack>
+
+class	RPN {
+
+public:
+	RPN(); // ocf default constructor
+	RPN(const RPN &src); // ocf copy constructor
+	~RPN(); // ocf destructor
+
+	RPN&	operator=(const RPN &src); // ocf copy assignment operator
+
+private:
+	std::stack<int>	_operands; // LIFO
+	char			_operator;
+};
